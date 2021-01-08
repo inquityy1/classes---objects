be68ed0c89bb607347f6877e21c3dd1019f0b8cf
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-08 14:14:39 +0100

    classes and objects project

---
+++ assets/scripts/app.js
@@ -0,0 +1,57 @@
+class Product {
+//	title = 'DEFAULT';
+//	imageUrl;
+//	description;
+//	price;
+	
+	constructor(title, image, desc, price) {
+		this.title = title;
+		this.imageUrl = image;
+		this.description = desc;
+		this.price = price;
+	}
+}
+
+class ProductList {
+	products = [
+		new Product(
+			'A Pillow',
+			'https://wallpaperaccess.com/full/1941443.jpg',
+			'A soft pillow!',
+			19.99
+		),
+		new Product(
+			'A Carpet',
+			'https://steamuserimages-a.akamaihd.net/ugc/940587658404822896/E5E0BC2DA2C74C08032227B339CA2AA2DCBE307B/',
+			'A carpet whitch you might like!',
+			89.99
+		)
+	];
+
+	constructor() {}	
+	
+	render() {
+		const renderHook = document.getElementById('app');
+		const prodList = document.createElement('ul');
+		prodList.className = 'product-list';
+		for (const prod of this.products) {
+			const prodEl = document.createElement('li');
+			prodEl.className = 'product-item';
+			prodEl.innerHTML = `
+				<div>
+					<img src="${prod.imageUrl}" alt="${prod.title}">
+					<div class="product-item__content">
+						<h2>${prod.title}</h2>
+						<h3>\$${prod.price}</h3>
+						<p>${prod.description}</p>
+						<button>Add to Cart</button>
+					</div>
+				</div>
+			`;
+			prodList.append(prodEl);
+		}
+		renderHook.append(prodList);
+	}
+}
+
+productList.render();
